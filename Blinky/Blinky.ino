272c88528637302e356fdc4e673da67d7fd31043
federepo <nubeingenieria@gmail.com>
2018-03-29 13:26:19 -0300

    Cambios en el .ino

--- Blinky/Blinky.ino
+++ Blinky/Blinky.ino
@@ -3,25 +3,21 @@ Programa Ejemplo: Blinky
 Arduino UNO
 ****************************************************************/
 
-#define Led_1 13   //En el pin 13 se encuentra el LED rojo
-#define Led_2 1    //En el pin 1 se encuentra el LED adicional
+#define Led_1 7   //En el pin 7 se conectará el LED 
 
 void setup() {
 
-  pinMode(Led_1, OUTPUT);     //Se configuran ambos pines como salida
-  pinMode(Led_2, OUTPUT);
+  pinMode(Led_1, OUTPUT);     //Se configura al pin 7 como salida
 
 }
 
 void loop() {
 
   digitalWrite(Led_1, HIGH);   // Se enciende el LED
-  digitalWrite(Led_2, HIGH);
   
-  delay(600);                       // demora de 600ms
+  delay(500);                       // demora de 600ms
   
   digitalWrite(Led_1, LOW);    // Se apaga el LED
-  digitalWrite(Led_1, LOW);
   
-  delay(600);                       // demora de 600ms
+  delay(500);                       // demora de 600ms
 }
