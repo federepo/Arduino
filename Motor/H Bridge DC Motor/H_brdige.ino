5d30cbd9645b8b7e3c4c0753d07cefaa763dab11
federepo <32501268+federepo@users.noreply.github.com>
2018-04-09 12:06:52 -0300

    Create H_brdige.ino

---
+++ Motor/H Bridge DC Motor/H_brdige.ino
@@ -0,0 +1,28 @@
+/****************************************************************
+Programa Ejemplo: Controlar sentido de Motor con L298
+Arduino UNO.
+
+Nota: Tabla de verdad que utiliza el L298:
+  INPUTS         FUNCTION   
+IN1    IN2        
+HIGH   LOW       FORWARD
+LOW    HIGH      REVERSE
+LOW    LOW        STOP
+****************************************************************/
+
+int IN1 = 1; 
+int IN2 = 2;
+
+void setup()
+{
+  pinMode (IN1, OUTPUT);    // Pin 1 que controla al puente H 
+  pinMode (IN2, OUTPUT);    // Pin 2 que controla al puente H
+}
+
+void loop()
+{
+   
+  digitalWrite (IN1, HIGH);   // Motor gira en un determinado sentido
+  digitalWrite (IN2, LOW); 
+  
+}
