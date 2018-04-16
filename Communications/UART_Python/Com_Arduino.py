448680d5e7711b569c78e11bc5a8ff0fdc24a86c
fgdangiolo <nubeingenieria@gmail.com>
2018-04-16 14:03:34 -0300

    subido

---
+++ Communications/UART_Python/Com_Arduino.py
@@ -0,0 +1,14 @@
+
+import serial, time
+
+com_arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+time.sleep(2)
+
+i=1
+
+while (i<2):
+    dato_arduino = com_arduino.readline()
+    print(dato_arduino)
+
+
