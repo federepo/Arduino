29fcbb6eff6405152c5a3729ea07fb0633752f14
fgdangiolo <fgdangiolo@gmail.com>
2020-02-09 19:22:37 -0300

    Subido

--- Codes_Library/C++/LM35.h
+++ Codes_Library/C++/LM35.h
@@ -1,9 +1,9 @@
 /* *****************************************************************
  * LM35 Library 
- * File: LM35.H
+ * File: LM35.h
  * VERSION: 1.0
  * Author: Federico Gabriel D'Angiolo
- * Description: This library contains all definitions about LM35
+ * Description: This file contains all definitions about LM35
 *******************************************************************/
 
 #ifndef LM35_h
@@ -11,22 +11,19 @@
 
 #include "Arduino.h"
 
-class LM35{
+class LM35
+{
 
   public:
 
       // constructor
       LM35(int pin);
 
-      // define methods
       // return temperature in celsius
-      float cels();   
+      float cels();  
+ 
       // return temperature in fahrenheit
       float fahr();     
-
-  private:
-      int _pin;
-
 };
 
 #endif
