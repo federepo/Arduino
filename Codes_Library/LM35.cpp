f1830190ccedcd357894a623a9aebb9409d3a2dc
fgdangiolo <fgdangiolo@gmail.com>
2020-02-09 19:26:22 -0300

    Subido

---
+++ Codes_Library/LM35.cpp
@@ -0,0 +1,26 @@
+/* *****************************************************************
+ * LM35 Library 
+ * File: LM35.cpp
+ * VERSION: 1.0
+ * Author: Federico Gabriel D'Angiolo
+ * Description: This file contains all functions about LM35
+*******************************************************************/
+
+#include "Arduino.h"
+#include "Morse.h"
+
+LM35::LM35()
+{
+}
+
+float LM35::cels()
+{
+   return tempC = (float(analogRead(_pin))*5/(1023))/0.01;
+}
+
+float LM35::fah()
+{
+   return tempF = (((float(analogRead(_pin))*5/(1023))/0.01)*1.8)+32;
+}
+
+
