08e9fe15b7fdd6d8a7f970edd78dd3ee67a1babf
fgdangiolo <fgdangiolo@gmail.com>
2020-07-06 23:04:15 -0300

    Subido

--- Communications/I2C/I2C.ino
+++ Communications/I2C/I2C.ino
@@ -1,1 +1,30 @@
+/******************************************** 
+ * I2C for Arduino
+/* Board I2C / TWI pins
 
+Uno, Ethernet A4 (SDA), A5 (SCL)
+Mega2560  20 (SDA), 21 (SCL)
+Leonardo  2 (SDA), 3 (SCL)
+Due 20 (SDA), 21 (SCL), SDA1, SCL1
+********************************************/
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(); // Initizializate I2C
+}
+
+byte data = 0xFF;
+
+void loop()
+{
+  Wire.beginTransmission(0x27); // Adress
+  Wire.write("x is ");        
+  Wire.write(data);           // send data  
+  Wire.endTransmission();    // stop transmitting
+  
+  delay(500);
+
+  
+}
